b56e41338b743472512552b756d0b1c164bac593
jackjieS <563255057@qq.com>
2019-12-21 06:12:11 +1100

    33

--- run.py
+++ run.py
@@ -1,19 +1,8 @@
-import configparser
 import json
 import os
 
 from Func.Porcess import process
 
-
-def readUser():
-    dictAccounts = []
-    cfg = configparser.ConfigParser()
-    cfg.read("cfg.ini")
-    for key in cfg["User information"]:
-        jsonData = json.loads(cfg["User information"][key])
-        account = (jsonData["Email"], jsonData["Passwd"])
-        dictAccounts.append(account)
-    return dictAccounts
 
 
 
@@ -21,7 +10,7 @@ if __name__ == "__main__":
     '''
     Chrome version = 79
     '''
-    dic = readUser()
-    p=process(dic[0][0],dic[0][1])
+
+    p=process("jackjie330@gmail.com","Majsoul3366662")
 
     p.run()
